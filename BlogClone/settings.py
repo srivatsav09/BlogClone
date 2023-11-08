3f5a6aa8ec342614269217ddf70a92c9278e90c7
Srivatsav <93873683+srivatsav09@users.noreply.github.com>
2023-11-08 11:31:49 +0530

    Update settings.py

--- BlogClone/settings.py
+++ BlogClone/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g=yk*^b%3fq0sti(kwvr754x6_6^%*siom%0m%5a4-+kwc59ad
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
